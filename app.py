52ac6fab81c25b0929523c3ff3dedc696a4cbf7f
dontv3x <alva.yt2010@gmail.com>
2025-08-05 14:52:03 +0100

    Fix port binding for render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def from_website():
     return render_template('index.html', messages=messages)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
