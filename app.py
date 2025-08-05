4c1511b83cd30a64f2fe5eddc0d49c89e764a475
dontv3x <alva.yt2010@gmail.com>
2025-08-05 13:58:11 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+# Store message temporarily
+messages = []
+
+@app.route('/')
+def home():
+    return render_template('index.html', messages=messages)
+
+@app.route('/api/send', methods=['POST'])
+def receive_data():
+    data = request.json
+    messages.append(data.get("message", "No message"))
+    return {"status": "success"}
+
+@app.route('/api/from-website', methods=['POST'])
+def from_website():
+    message = request.form.get("info")
+    messages.append(message)
+    return render_template('index.html', messages=messages)
+
+if __name__ == '__main__':
+    app.run(debug=True)
